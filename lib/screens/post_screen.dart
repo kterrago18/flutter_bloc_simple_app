4aed8e4db075dfd5674ffea0dd682427d8092e0f
Kenneth Terrago <kterrago091893@gmail.com>
2022-03-16 09:48:45 +0800

    :tada: initial commit

---
+++ lib/screens/post_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostScreen extends StatelessWidget {
+  const PostScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post Screen'),
+      ),
+      body: const Center(child: Text('Post Screen')),
+    );
+  }
+}
